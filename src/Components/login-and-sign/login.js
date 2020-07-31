330a5e74acde8cc653f986c4e29e4711bc76741a
jarrodskahill <jarrodskahill@gmail.com>
2020-07-31 04:36:34 -0700

    finished adding sign up page

--- src/Components/login-and-sign/login.js
+++ src/Components/login-and-sign/login.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Axios from "axios";
 
-const Login = () => {
+const Login = (props) => {
+  const [userCred, setUserCred] = useState({
+    displayName: "",
+    password: "",
+  });
+
+  const handleLogin = (e) => {
+    setUserCred({
+      ...userCred,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    Axios.post(`http://localhost:7000/user/login`, userCred)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/profile");
+        window.location.reload();
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <form className="login">
+    <form onSubmit={onSubmit} className="login">
       <h1>Login</h1>
       <span>
         <h3>Username:</h3>
-        <input className="inputs" />
+        <input
+          name="displayName"
+          value={userCred.displayName}
+          onChange={handleLogin}
+          className="inputs"
+        />
       </span>
       <span>
         <h3>Password:</h3>
-        <input className="inputs" />
+        <input
+          name="password"
+          value={userCred.password}
+          onChange={handleLogin}
+          className="inputs"
+        />
       </span>
+      <button className="regButton">Login</button>
       <h3>
         Dont have an account? <Link to="signup">Sign up!</Link>
       </h3>
--- src/Components/login-and-sign/signUp.js
+++ src/Components/login-and-sign/signUp.js
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Axios from "axios";
 
-const SignUp = () => {
+const SignUp = (props) => {
   const [form, setForm] = useState({
-    username: "",
+    displayName: "",
     password: "",
   });
+
+  const handleSignUp = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    Axios.post(`http://localhost:7000/user/register`, form)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/profile");
+        window.location.reload();
+      })
+      .catch((err) => console.error(err));
+  };
   return (
-    <div className="login">
+    <form onSubmit={onSubmit} className="login">
       <h1>Sign Up</h1>
       <span className="signupSpan">
         <b>Select a username</b>
-        <input />
+        <input
+          name="displayName"
+          value={form.displayName}
+          onChange={handleSignUp}
+        />
       </span>
       <span className="signupSpan">
         <b>Select a password</b>
-        <input />
+        <input name="password" onChange={handleSignUp} value={form.password} />
       </span>
+      <button className="regButton">Sign Up</button>
       <h3>
         Already a User? <Link to="/login">Login</Link>
       </h3>
-    </div>
+    </form>
   );
 };
 
