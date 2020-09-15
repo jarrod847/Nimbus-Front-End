abf292593f34c39d21c7be35a7299a00f7dd0aba
jarrodskahill <jarrodskahill@gmail.com>
2020-09-14 21:27:51 -0700

    added new credentials for sign up

--- src/Components/login-and-sign/signUp.js
+++ src/Components/login-and-sign/signUp.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Axios from "axios";
+import { useRecoilState } from "recoil";
+import User from "../Recoil/atom/user";
 
 const SignUp = (props) => {
   const [form, setForm] = useState({
     displayName: "",
     password: "",
+    bio: "",
+    img: "",
   });
-
+  const [userProfile, setUserProfile] = useRecoilState(User);
   const handleSignUp = (e) => {
     setForm({
       ...form,
@@ -21,25 +25,46 @@ const SignUp = (props) => {
       .then((res) => {
         localStorage.setItem("token", res.data.payload);
         props.history.push("/profile");
-        window.location.reload();
+        setUserProfile(res);
+        localStorage.setItem("info", res.data);
       })
       .catch((err) => console.error(err));
   };
   return (
-    <form onSubmit={onSubmit} className="login">
+    <form onSubmit={onSubmit} className="signUp">
       <h1>Sign Up</h1>
-      <span className="signupSpan">
-        <b>Select a username</b>
-        <input
-          name="displayName"
-          value={form.displayName}
-          onChange={handleSignUp}
-        />
-      </span>
-      <span className="signupSpan">
-        <b>Select a password</b>
-        <input name="password" onChange={handleSignUp} value={form.password} />
-      </span>
+      <div className="signUpCred">
+        <span className="signupSpan">
+          <b>Select a username</b>
+          <input
+            name="displayName"
+            value={form.displayName}
+            onChange={handleSignUp}
+          />
+        </span>
+        <span className="signupSpan">
+          <b>Select a password</b>
+          <input
+            name="password"
+            onChange={handleSignUp}
+            value={form.password}
+          />
+        </span>{" "}
+        <span className="signupSpan">
+          <b>Img</b>
+          <input name="img" onChange={handleSignUp} value={form.img} />
+        </span>{" "}
+        <span className="signupSpan">
+          <b>Bio</b>
+          <textarea
+            className="bioText"
+            name="bio"
+            onChange={handleSignUp}
+            value={form.bio}
+          />
+        </span>
+      </div>
+
       <button className="regButton">Sign Up</button>
       <h3>
         Already a User? <Link to="/login">Login</Link>
--- src/Components/user/profile.js
+++ src/Components/user/profile.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import User from "../Recoil/atom/user";
 
 const Profile = (props) => {
   const logout = () => {
@@ -7,7 +9,8 @@ const Profile = (props) => {
     props.history.push("/login");
     window.location.reload();
   };
-
+  const userInfo = useRecoilValue(User);
+  console.log(userInfo);
   const user = localStorage.getItem("User");
   const bio = localStorage.getItem("Bio");
   const img = localStorage.getItem("img");
