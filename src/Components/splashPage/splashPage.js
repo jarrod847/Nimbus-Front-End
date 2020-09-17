f69105f350cc15a858e91d46b178855681323611
jarrodskahill <jarrodskahill@gmail.com>
2020-09-17 05:49:02 -0700

    added some basic text to homepage

--- src/Components/splashPage/splashPage.js
+++ src/Components/splashPage/splashPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { splashText } from "./splashText";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div class="splash">
+      <h1>Welcome to Nimbus</h1>
+      <p>{splashText[0]}</p>
+      <h2>Rewards System</h2>
+      <p>{splashText[1]}</p>
     </div>
   );
 };
