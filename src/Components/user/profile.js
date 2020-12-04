202338d1466b84957ed744f9a9f8e0ec2d81879f
jarrodskahill <jarrodskahill@gmail.com>
2020-12-03 19:42:39 -0600

    profile now displays users posts

--- src/Components/user/profile.js
+++ src/Components/user/profile.js
@@ -1,31 +1,45 @@
 import Axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import User from "../Recoil/atom/user";
+import Post from "../timeLine/post";
 
 const Profile = (props) => {
+  const [cloudThoughts, setCloudThoughts] = useState([]);
   const logout = () => {
     localStorage.clear();
     props.history.push("/login");
   };
   const userInfo = useRecoilValue(User);
-  console.log(userInfo);
+  const userId = userInfo.id;
+  console.log(userId);
 
-  // const userPosts = () => {
-  //   Axios.get(`${process.env.REACT_APP_API_URL}/post/userposts/`)
-  // }
+  useEffect(() => {
+    Axios.get(`${process.env.REACT_APP_API_URL}post/userposts/${userId}`)
+      .then((res) => {
+        setCloudThoughts(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div className="profile">
-      <div>
-        <img src={userInfo.img} />
+    <div>
+      <div className="profile">
+        <div>
+          <img src={userInfo.img} />
+        </div>
+        <div className="userInfo">
+          <h3>Display Name: {userInfo.displayName}</h3>
+          <p>Bio: {userInfo.bio}</p>
+          <Link>Edit Profile</Link>
+          <button onClick={logout}>Logout</button>
+        </div>
       </div>
-      <div className="userInfo">
-        <h3>Display Name: {userInfo.displayName}</h3>
-        <p>Bio: {userInfo.bio}</p>
-        <Link>Edit Profile</Link>
-        <button onClick={logout}>Logout</button>
+      <div className="UsersPosts">
+        {cloudThoughts.map((posts) => (
+          <Post key={posts.id} post={posts} user={userInfo} />
+        ))}
       </div>
     </div>
   );
