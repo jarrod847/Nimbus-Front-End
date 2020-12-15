e0cf6f1fe859c6c737f34caadc8d76cea5bb97bc
Your name hereSaleem <saleemsplace@aol.com>
2020-12-14 19:51:52 -0800

    style

---
+++ src/Components/Comments/comments.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Axios from "axios";
+import { useRecoilState } from "recoil";
+import User from "../Recoil/atom/user";
+import "./comments.css";
+
+
+
+const Comments = (props) => {
+
+  return (
+      <div>
+        <div className="cloud"></div> 
+          
+
+</div>
+
+
+  );
+};
+
+
+export default Comments;
