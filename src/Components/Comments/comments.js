266f9b048f85b872b0c859ca91cef5f52ca47fdf
Your name hereSaleem <saleemsplace@aol.com>
2020-12-15 19:38:19 -0800

    trying to get user image

--- src/Components/Comments/comments.js
+++ src/Components/Comments/comments.js
@@ -4,21 +4,36 @@ import Axios from "axios";
 import { useRecoilState } from "recoil";
 import User from "../Recoil/atom/user";
 import "./comments.css";
+import Post from "../timeLine/post";
+import Data, { data } from "../timeLine/data";
+import Profile from "./Components/user/profile";
 
 
 
-const Comments = (props) => {
 
+function Comments(props) {
+  console.log({props})
+
+  const userInfo=data
   return (
-      <div>
-        <div className="cloud"></div> 
-          
+    <div>
+      <div className='cloudContainer'>
+        <div className='cloud'></div>
+        <div className="cloudBottom"></div>
+        <div className="cloudBig">
+          <img src={data.userImg} />
+          </div>
+        <div className="cloudSmall"></div>
+        <div className="cloudExSmall"></div>
+      </div>
 
-</div>
+
+
+    </div>
 
 
   );
-};
+}
 
 
 export default Comments;
