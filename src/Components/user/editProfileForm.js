9101948c17183fa35ebb38c67c3658b011b3225b
jarrodskahill <jarrodskahill@gmail.com>
2021-02-02 20:24:38 -0600

    can now set edited info to state

--- src/Components/user/editProfileForm.js
+++ src/Components/user/editProfileForm.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import User from "../Recoil/atom/user";
 
-const EditProfileInfo = ({ userInfo }) => {
+const EditProfileInfo = ({ userInfo, bool }) => {
+  const [newInfo, setNewinfo] = useRecoilState(User);
+  const [info, setInfo] = useState({
+    displayName: userInfo.displayName,
+    bio: userInfo.bio,
+    img: userInfo.img,
+  });
+
+  const handleChange = (e) => {
+    setInfo({
+      ...info,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const setToFalse = () => {
+    setNewinfo(info);
+    bool(false);
+  };
+
   return (
     <div>
       <div>
         <label>Display Name</label>
-        <input value={userInfo.displayName} />
+        <input
+          name="displayName"
+          value={info.displayName}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label>Bio</label>
-        <input value={userInfo.bio} />
+        <input name="bio" value={info.bio} onChange={handleChange} />
       </div>
       <div>
         <label>Picture</label>
-        <input value={userInfo.bio} />
+        <input name="img" value={info.img} onChange={handleChange} />
       </div>
+      <button onClick={setToFalse}>Done</button>
     </div>
   );
 };
